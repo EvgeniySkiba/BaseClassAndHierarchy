b4ac8012e3abed53c6b4e911658175e1c8880c21
EvgeniySkiba <Skiba.Evgeniy@hotmail.com>
2018-05-14 00:45:15 +0300

    completed first version

--- BaseClassAndHierarchy/main.cpp
+++ BaseClassAndHierarchy/main.cpp
@@ -22,21 +22,27 @@ int main()
 	Student student("1", "2", "3", 4, "group name", 5);
 	//student.print();
 
-	Family *  stud1 = new Family("ф", "имя", "отч", 16, "имя группы", 4, 3500);
-	Family *  stud2 = new Family("ф2", "имя2", "отч", 16, "имя группы", 4, 1800);
-	Family *  stud3 = new Family("ф3", "имя2", "отч", 16, "имя группы 2 ", 4, 800);
+	Family *  stud1 = new Family("фамилия ", "имя", "Отчество", 16, "группа ", 4, 3500);
+	Family *  stud2 = new Family("фамилия", "имя2", "Отчество 2", 16, " группа 3", 4, 1800);
+	Family *  stud3 = new Family("фамилия", "имя3", "Отчество 3", 18, " группа  4 ", 4, 800);
 
-	Student *  stud4 = new Student();
-	Student *  stud5 = new Student();
-	Student *  stud6 = new Student();
-	Student *  stud7 = new Student();
+	Family *  stud4 = new Family("фамилия", "имя4", "Отчество 4", 19, " группа 4", 4, 5800);
+	Family *  stud5 = new Family("фамилия", "имя5", "Отчество 5", 20, " группа 5 ", 4, 6800);
+	Family *  stud6 = new Family("фамилия", "имя6", "Отчество 6", 21, " группа 5 ", 4, 2800);
+	Family *  stud7 = new Family("фамилия", "имя7", "Отчество 7", 20, " группа 5 ", 4, 1800);
+	Family *  stud8 = new Family("фамилия", "имя8", "Отчество 8", 21, " группа   ", 4, 1800);
+	
 
-	Family * famillyArray = new Family[3];
+	Family * famillyArray = new Family[8];
 
 	famillyArray[0] = *stud1;
 	famillyArray[1] = *stud2;
 	famillyArray[2] = *stud3;
-
+	famillyArray[3] = *stud4;
+	famillyArray[4] = *stud5;
+	famillyArray[5] = *stud6;
+	famillyArray[6] = *stud7;
+	famillyArray[7] = *stud8;
 
 	Hostel * hostel = new Hostel(famillyArray);
 
